f07a092d407c13b5ad2505283add684527f11e35
Stefan Berger <stefanb@linux.vnet.ibm.com>
2017-07-17 16:42:00 -0400

    Consider virtualized xattrs in one more function
    Fix the function vfs_getxattr_alloc to properly determine the
    xattr to read and then read its contents into an allocated
    buffer.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

--- fs/xattr.c
+++ fs/xattr.c
@@ -387,7 +387,7 @@ xattr_list_userns_rewrite(char *list, ssize_t size, size_t list_maxlen)
 	ssize_t d_off;
 	char *name, *newname;
 
-	if (!list || size < 0 || current_user_ns() == &init_user_ns)
+	if (current_user_ns() == &init_user_ns)
 		return size;
 
 	if (size) {
@@ -809,33 +809,58 @@ ssize_t
 vfs_getxattr_alloc(struct dentry *dentry, const char *name, char **xattr_value,
 		   size_t xattr_size, gfp_t flags)
 {
-	const struct xattr_handler *handler;
 	struct inode *inode = dentry->d_inode;
 	char *value = *xattr_value;
-	int error;
+	const struct xattr_handler *handler = NULL;
+	char *newname =  NULL;
+	const char *nname;
+	int userns_supt_xattr, error;
+	struct user_namespace *userns = current_user_ns();
 
 	error = xattr_permission(inode, name, MAY_READ);
 	if (error)
 		return error;
 
-	handler = xattr_resolve_name(inode, &name);
-	if (IS_ERR(handler))
-		return PTR_ERR(handler);
-	if (!handler->get)
-		return -EOPNOTSUPP;
-	error = handler->get(handler, dentry, inode, name, NULL, 0);
-	if (error < 0)
-		return error;
+	userns_supt_xattr = (xattr_is_userns_supported(name, false) >= 0);
 
-	if (!value || (error > xattr_size)) {
-		value = krealloc(*xattr_value, error + 1, flags);
-		if (!value)
-			return -ENOMEM;
-		memset(value, 0, error + 1);
-	}
+	do {
+		kfree(newname);
 
-	error = handler->get(handler, dentry, inode, name, value, error);
-	*xattr_value = value;
+		newname = xattr_userns_name(name, userns);
+		if (IS_ERR(newname))
+			return PTR_ERR(newname);
+
+		nname = newname;
+		handler = xattr_resolve_name(inode, &nname);
+		if (IS_ERR(handler)) {
+			error = PTR_ERR(handler);
+			goto out;
+		}
+		if (!handler->get) {
+			error = -EOPNOTSUPP;
+			goto out;
+		}
+		error = handler->get(handler, dentry, inode, nname, NULL, 0);
+		if (error > 0) {
+			if (!value || (error > xattr_size)) {
+				value = krealloc(*xattr_value, error + 1,
+						 flags);
+				if (!value) {
+					error = -ENOMEM;
+					goto out;
+				}
+			}
+			error = handler->get(handler, dentry, inode, nname,
+					     value, error);
+			*xattr_value = value;
+
+			goto out;
+		}
+		userns = userns->parent;
+	} while ((error == -ENODATA) && userns && userns_supt_xattr);
+
+out:
+	kfree(newname);
 	return error;
 }
 
